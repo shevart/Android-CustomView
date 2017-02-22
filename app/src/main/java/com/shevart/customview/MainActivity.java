7c3c627dcfbd1e4d62d4ad9c8ce0392398c3ae69
Artem Shevchenko <shevartsoft@gmail.com>
2017-02-22 18:30:55 +0200

    test push

--- app/src/main/java/com/shevart/customview/MainActivity.java
+++ app/src/main/java/com/shevart/customview/MainActivity.java
@@ -11,7 +11,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private Handler handler = new Handler(Looper.getMainLooper());
-
     private UserProgressCircle userProgressCircle;
 
     @Override
