80a19c58685d275b28e3df97253cd1db6b558205
Artem Shevchenko <shevartsoft@gmail.com>
2017-10-17 17:19:20 +0300

    add new module for circle views section

---
+++ circleviews/src/main/java/com/shevart/circleviews/SimpleCircleProgressBar.java
@@ -0,0 +1,22 @@
+package com.shevart.circleviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SimpleCircleProgressBar extends View {
+
+    public SimpleCircleProgressBar(Context context) {
+        super(context);
+    }
+
+    public SimpleCircleProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SimpleCircleProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    
+
+}
