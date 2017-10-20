b3fd877cd878c5977970ab32fa82276f5ed3e1f0
Artem Shevchenko <shevartsoft@gmail.com>
2017-10-20 14:36:08 +0300

    start work with TripleCircleProgressBar

---
+++ circleviews/src/main/java/com/shevart/circleviews/triple_circle_progress_bar/TripleCircleProgressBar.java
@@ -0,0 +1,21 @@
+package com.shevart.circleviews.triple_circle_progress_bar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+
+import com.shevart.circleviews.BaseCircleView;
+
+public class TripleCircleProgressBar extends BaseCircleView {
+    public TripleCircleProgressBar(Context context) {
+        super(context);
+    }
+
+    public TripleCircleProgressBar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TripleCircleProgressBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
