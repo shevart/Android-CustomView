4012119eb39fc42c861a0e06dbc8710f7fddf5f4
Artem Shevchenko <shevartsoft@gmail.com>
2017-10-24 16:47:57 +0300

    add screens to demo circleViews topic

---
+++ app/src/main/java/com/shevart/customview/screens/circleviewstopic/CircleViewsTopicDemoActivity.java
@@ -0,0 +1,15 @@
+package com.shevart.customview.screens.circleviewstopic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shevart.customview.R;
+
+public class CircleViewsTopicDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_views_topic_demo);
+    }
+}
